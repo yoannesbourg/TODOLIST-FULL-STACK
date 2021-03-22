4444b9c737f384bd65d01fe4aa6473368c41ac96
Yoann <yoannpascalbouix@gmail.com>
2021-03-22 12:55:19 +0100

    Create new pool

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const pool = require("./db")
 
 
 //middleware
 app.use(cors())
 app.use(express.json())
+
+//ROUTES
+
+//Create a todo
+
+//get all todos
+
+//get a todo
+
+//update a todo
 
 app.listen(5000, () => {
     console.log("server has started on port 5000")
