0ed4407706bb0a44139a8473907c3da7be96797c
Yoann <yoannpascalbouix@gmail.com>
2021-03-22 20:39:31 +0100

    Get all todos

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -1,29 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Input from '../Input/Input'
 import Item from '../Item/Item'
 
 import '../../App.css'
 
-const todolist = {
+const item = {
     id: 1,
     text: 'Buy bananas',
     checked: true
 }
 
 const List = () => {
-  
+
+    const [todos, setTodos] = useState([])
+
+    const getTodos = async () => {
+        try {
+            const response = await fetch("http://localhost:5000/todos")
+            const jsonData = await response.json()
+            setTodos(jsonData)
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    useEffect(() => {
+        getTodos()
+    }, [])
+
     return (
       <>
         <Input />
         <ul className='list'>
-          {todolist.map(item => (
-            <Item
-              key={item.id}
-              id={item.id}
-              text={item.text}
-              checked={item.checked}
-            />
-          ))}
+            {todos.map(item => (
+                <Item
+                key={item.id}
+                id={item.id}
+                description={item.description}
+                checked={false}
+              />
+            ))}
         </ul>
       </>
     )
