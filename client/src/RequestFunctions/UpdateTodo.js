794c8491e0e9b0682b26256f4279e7e0c7770efa
Yoann <yoannpascalbouix@gmail.com>
2021-03-25 19:11:20 +0100

    Clean up

--- client/src/RequestFunctions/UpdateTodo.js
+++ client/src/RequestFunctions/UpdateTodo.js
@@ -2,7 +2,7 @@ export const updateTodo = async ( e,description, id, handleEditFunction) => {
     e.preventDefault()
     try {
         const body = { description }
-        const response = await fetch(`http://localhost:5000/todos/${id}`, {
+        await fetch(`http://localhost:5000/todos/${id}`, {
         method: "PUT",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(body)
